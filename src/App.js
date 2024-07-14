9dd30e5651504f1a6abd365c9b6c55ee3ae70d27
anjaliKolhatkar <anjalikolhatkar1803@gmail.com>
2024-07-14 22:18:27 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Components/Calculator';
+ 
+ 
+function App() {
+  return (
+     <>
+     <h1 className = "heading">Calculator</h1>
+   <Calculator/>
+     </>
+  );
+}
+
+export default App;
